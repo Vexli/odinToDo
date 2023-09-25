c14510c807211e1716d4561ea0c3d19c0b6e31d3
Vexli <74198817+Vexli@users.noreply.github.com>
2023-09-25 16:05:04 +0200

    Show / Hide Todo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 /* IMPORT */
 import './style.css';
 import arrToDo from './varGlobal.js';
-import { arrSearch } from './search.js';
-import { addFormDate, cnvrtDateISO } from './date.js';
-import { modeDark } from './modeDark.js';
-import { todoDom, createToDo, todoVar } from './createToDo.js';
-import { addEleAfter, addEleBefore, delToDo, displayElement, getEle } from './alterDOM.js';
-import { addDate, addDesc, addPrio, addProject, setAttribute } from './alterToDo.js';
+import {
+  arrSearch,
+  searchDisplay
+} from './search.js';
+import {
+  addFormDate,
+  cnvrtDateISO
+} from './date.js';
+import {
+  modeDark
+} from './modeDark.js';
+import {
+  todoDom,
+  createToDo,
+  todoVar
+} from './createToDo.js';
+import {
+  addEleAfter,
+  addEleBefore,
+  delToDo,
+  displayElement,
+  getEle
+} from './alterDOM.js';
+import {
+  addDate,
+  addDesc,
+  addPrio,
+  addProject,
+  setAttribute
+} from './alterToDo.js';
 
 /* VARIABLE Define */
 const dateToday = new Date();
@@ -22,32 +46,34 @@ divAdd.style.display = "block";
 /* FUNCTION Define */
 
 /* FUNCTION Call */
-addFormDate(dateTodo,dateToday,365,365);
+addFormDate(dateTodo, dateToday, 365, 365);
 
 /* Listener */
 // General function listenenr
 document.addEventListener("click", (e) => {
-  if (e.target.id == "addTodo"){
+  if (e.target.id == "addTodo") {
     displayElement(divAdd);
     displayElement(formAdd);
-  } else if (e.target.classList.contains("todo") == true){
-  } else if (e.target.id == "nav"){
+  } else if (e.target.classList.contains("todo") == true) {} else if (e.target.id == "nav") {
     modeDark();
   }
 });
 
 // Listen for submission of search
-formSearch.addEventListener("submit", function(event){
+formSearch.addEventListener("submit", function(event) {
   event.preventDefault();
   let searchAttribute = formSearch.elements.attribute.value;
   let searchValue = formSearch.elements.value.value;
-  arrSearch(arrToDo,searchAttribute,searchValue);
+  console.log(arrToDo);
+  let resultSearch = arrSearch(arrToDo, searchAttribute, searchValue);
+  console.log(resultSearch);
+  searchDisplay(arrToDo,resultSearch);
 });
 
 // Listen for submission of new ToDo
-formAdd.addEventListener("submit", function(event){
+formAdd.addEventListener("submit", function(event) {
   event.preventDefault();
-  createToDo("container",formAdd.elements.todoTitel.value,formAdd.elements.todoDate.value,formAdd.elements.todoDesc.value,formAdd.elements.todoPriority.value,formAdd.elements.todoProject.value)
+  createToDo("container", formAdd.elements.todoTitel.value, formAdd.elements.todoDate.value, formAdd.elements.todoDesc.value, formAdd.elements.todoPriority.value, formAdd.elements.todoProject.value);
   formAdd.style.display = "none";
   divAdd.style.display = "block";
 });
--- src/search.js
+++ src/search.js
@@ -1,12 +1,28 @@
+/* IMPORT */
+import { getEle } from './alterDOM.js';
+
 // Search through array
 export function arrSearch(arr,attribute,value){
   let arrResult = [];
   for (let i = 0; i < arr.length; i++){
     if (arr[i][attribute] == value){
-      console.log(arr[i][attribute],value)
-      arrResult.push([i,arr[i].id]);
+      console.log(arr[i][attribute],value);
+      arrResult.push(arr[i]);
       console.log(arrResult);
     }
   }
-  return arrResult
+  return arrResult;
+}
+
+// Show / Hide Blocks
+export function searchDisplay(arr,arrTarget){
+    for (let i = 0; i < arr.length; i++) {
+      let ele = getEle(arr[i]);
+      console.log(arr,arrTarget,arrTarget.includes(arr[i]));
+      if (arrTarget.includes(arr[i])){
+        ele.style.display = "flex";
+      } else {
+        ele.style.display = "none";
+      }
+    }
 }
